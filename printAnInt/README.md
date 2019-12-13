d3328486e4a2de45dda3cb9d9ef8d93b3efc4c7e
Nicholas O'Kelley <leadslinger32@gmail.com>
2019-12-12 21:49:04 -0500

    Updating the repo && adding printAnInt

---
+++ printAnInt/README.md
@@ -0,0 +1,5 @@
+# Program to Print an Integer
+
+
+This is an example of a program that takes user input
+and prints it to the screen.
---
+++ printAnInt/printAnInt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// An example of how to print an integer given by the user.
+int main(){
+	
+	int number;
+
+	printf("Enter an integer: ");
+
+	//reads and stores input
+	scanf("%d", &number);
+
+	//displays output
+	printf("You entered: %d", number);
+
+	return 0;
+}
+
