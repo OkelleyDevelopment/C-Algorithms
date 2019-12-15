385c7bcd93e18f360b8bb38aeb4a52e28a0f0618
Nicholas O'Kelley <leadslinger32@gmail.com>
2019-12-14 23:40:44 -0500

    Updating the commit log

---
+++ examples/DateInLegal/date.c
@@ -0,0 +1,10 @@
+/* Prints a date in legal form */
+
+#include <stdio.h>
+
+int main(void){
+
+	int month, day, year;
+
+	printf("Enter date (mm/dd/yyyy): ");
+	scanf
---
+++ examples/FahrToCel/celsius.c
@@ -0,0 +1,24 @@
+/* Converts a Fahrenheit temperature to Celsius */
+
+#include <stdio.h>
+
+#define FREEZING_PT 32.0f
+
+/* Use the float version so that zero is not returned for scale factor */
+#define SCALE_FACTOR (5.0f / 9.0f)
+
+int main(void){
+
+	float fahrenheit, celsius;
+
+	printf("Enter Fahrenheit temperature: ");
+
+	scanf("%f", &fahrenheit);
+
+	celsius = (fahrenheit - FREEZING_PT) * SCALE_FACTOR;
+
+	printf("Celsius equivalent: %.1f\n", celsius);
+
+	return 0;
+}
+
