d2b093d4cfbf291accb48dab8b326a42733a7a58
Nicholas O'Kelley <leadslinger32@gmail.com>
2019-12-15 23:28:32 -0500

    Began writing the program

--- examples/DateInLegal/date.c
+++ examples/DateInLegal/date.c
@@ -7,4 +7,19 @@ int main(void){
 	int month, day, year;
 
 	printf("Enter date (mm/dd/yyyy): ");
-	scanf
+	scanf("%d / %d / %d", &month, &day, &year);
+
+	printf("Dated this %d", day);
+	switch(day){
+		case 1: case 21: case 31:
+			printf("st"); break;
+		case 2: case 22: 
+			printf("nd"); break;
+		case 3: case 23:
+			printf("rd"); break;
+		default: printf("th"); break;
+	}
+	switch(month){
+		case 1: printf("January"); break;
+		case 2: printf("February"); break;
+		case 3: printf("March"); break;
