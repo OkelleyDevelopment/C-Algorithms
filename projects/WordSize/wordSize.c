94985fca9ba1b0538bee71a1c6f35e0274e95aba
Nicholas O'Kelley <leadslinger32@gmail.com>
2020-01-11 21:32:40 -0500

    Finished ch12 project 1

---
+++ projects/WordSize/wordSize.c
@@ -0,0 +1,60 @@
+/*
+ * Author: Nicholas O'Kelley
+ *
+ * Write a program to check the "smallest" and "largest" word in a series of words.
+ * After the user enters the words, the program will determine which words
+ * would come first and last if the words were listed in dictionary order. The program
+ * must stop accepting input when the user entes a four-letter word. Assume that no word
+ * is more than 20 lettes long.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 20
+
+void read_line(char str[], int n);
+
+int main(void){
+
+	char curr_word[SIZE + 1];
+	char smallest[SIZE + 1];
+	char largest[SIZE + 1];
+
+	
+	printf("Enter a word: ");
+	read_line(curr_word, SIZE);
+	strcpy(smallest, strcpy(largest, curr_word));
+
+	while(strlen(curr_word) != 4){
+		printf("Enter a word: ");
+		read_line(curr_word, SIZE);
+		
+		if(strcmp(curr_word, smallest) < 0){
+			strncpy(smallest, curr_word, SIZE);
+		}
+		if(strcmp(curr_word, largest) > 0){
+			strncpy(largest, curr_word, SIZE);
+		}
+	}
+
+	printf("\nSmallest word: %s\n", smallest);
+	printf("Largest word: %s\n", largest);
+
+	return 0;
+}
+
+void read_line(char str[], int n){
+	int ch, i = 0;
+
+	while((ch = getchar()) != '\n'){
+		if(i < n){
+			str[i++] = ch;
+		}
+	}
+	str[i] = '\0';
+}
+
+
+
+
